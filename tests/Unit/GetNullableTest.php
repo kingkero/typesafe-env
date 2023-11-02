e60da3c8dcf23e57c79d413fe7e4704e05ee9122
Martin Rehberger <dev@kero.codes>
2023-11-02 18:46:56 +1100

    :sparkles: Add nullable tests for getNullableInt

--- tests/Unit/GetNullableTest.php
+++ tests/Unit/GetNullableTest.php
@@ -11,16 +11,19 @@ beforeAll(function () {
 it('should return null for a variable with content "null" and using string getter', function () {
     expect(Env::getNullableString('TEST_NULL'))->toBeNull();
     expect(Env::getNullableBool('TEST_NULL'))->toBeNull();
+    expect(Env::getNullableInt('TEST_NULL'))->toBeNull();
 });
 
 it('should return null for a variable with content "(null)" and using string getter', function () {
     expect(Env::getNullableString('TEST_NULL_BRACKETED'))->toBeNull();
     expect(Env::getNullableBool('TEST_NULL_BRACKETED'))->toBeNull();
+    expect(Env::getNullableInt('TEST_NULL_BRACKETED'))->toBeNull();
 });
 
 it('should default to null for an undefined variable', function () {
     expect(Env::getNullableString('UNDEFINED'))->toBeNull();
     expect(Env::getNullableBool('UNDEFINED'))->toBeNull();
+    expect(Env::getNullableInt('UNDEFINED'))->toBeNull();
 });
 
 it('should throw an InvalidTypeException for a bool variable when using getNullableString', function () {
@@ -30,3 +33,7 @@ it('should throw an InvalidTypeException for a bool variable when using getNulla
 it('should throw an InvalidTypeException for a string variable when using getNullableBool', function () {
     Env::getNullableBool('TEST_STRING');
 })->throws(InvalidTypeException::class, 'Expected `boolean|null` but received `string`.');
+
+it('should throw an InvalidTypeException for a string variable when using getNullableInt', function () {
+    Env::getNullableInt('TEST_STRING');
+})->throws(InvalidTypeException::class, 'Expected `int|null` but received `unknown`.');
